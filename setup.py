2f796430737bb2bd157468274b22bd30aa00e32a
Thomas Feldmann <feldmann.thomas@googlemail.com>
2014-06-27 21:39:47 +0200

    version push

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='raumfeld',
     packages=['raumfeld'],  # this must be the same as the name above
-    version='0.1',
+    version='0.2',
     install_requires=['pysimplesoap'],
     license='MIT',
     description='A pythonic library for discovering and controlling Teufel '
@@ -12,7 +12,6 @@ setup(
     author='Thomas Feldmann',
     author_email='feldmann.thomas@gmail.com',
     url='https://github.com/tfeldmann/python-raumfeld',
-    download_url='https://github.com/tfeldmann/python-raumfeld/tarball/0.1',
     keywords=['raumfeld', 'upnp', 'soap'],
     classifiers=[],
 )
